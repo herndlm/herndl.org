0a90dcb5b042c750b4303e82214ec320b9d1284c
Martin Herndl <martin@herndl.org>
2020-04-05 17:30:31 +0200

    Minify HTML via htmlminify, cleanup build scripts

--- compile.sh
+++ compile.sh
@@ -4,17 +4,17 @@ IFS=$'\n\t'
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly BODY=$(markdown "${DIR}/index.md")
-readonly DIST_DIR="dist/"
+readonly DIST_DIR="${DIR}/dist"
+readonly TMP_DIR="${DIR}/tmp"
 
-readonly OUTPUT_FILE="${DIR}/dist/index.html"
+readonly OUTPUT_FILE="${TMP_DIR}/index.html"
 
-rm -rf "${DIST_DIR}"
-mkdir "${DIST_DIR}"
+rm -rf "${DIST_DIR}" "${TMP_DIR}"
+mkdir "${DIST_DIR}" "${TMP_DIR}"
 
 cat "${DIR}/template_before.html" > "${OUTPUT_FILE}"
 echo "${BODY}" >> "${OUTPUT_FILE}"
 cat "${DIR}/template_after.html" >> "${OUTPUT_FILE}"
-tidy -modify "${OUTPUT_FILE}"
 
 cp assets/images/* "${DIST_DIR}"
 find assets -maxdepth 1 -type f -exec cp -t "${DIST_DIR}" {} +
