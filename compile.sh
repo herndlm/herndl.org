d0b27eb47c3dc20eb81ea497073b8d24a83fecf6
Martin Herndl <martin@herndl.org>
2020-04-05 15:26:18 +0200

    Use babel, optimize assets, add missing files

--- compile.sh
+++ compile.sh
@@ -2,14 +2,20 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly BODY=$(markdown "${DIR}/index.md")
-readonly OUTPUT_FILE="${DIR}/index.html"
+readonly DIST_DIR="dist/"
+
+readonly OUTPUT_FILE="${DIR}/dist/index.html"
+
+rm -rf "${DIST_DIR}"
+mkdir "${DIST_DIR}"
 
 cat "${DIR}/template_before.html" > "${OUTPUT_FILE}"
 echo "${BODY}" >> "${OUTPUT_FILE}"
 cat "${DIR}/template_after.html" >> "${OUTPUT_FILE}"
-
 tidy -modify "${OUTPUT_FILE}"
+
+cp assets/images/* "${DIST_DIR}"
+find assets -maxdepth 1 -type f -exec cp -t "${DIST_DIR}" {} +
 
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 # Martin Herndl
-<img alt="profile image" src="https://herndl.org/herndlm.jpg" width="150" height="150" />
+<img alt="profile image" src="profileimage_square_150.jpg" width="150" height="150" id="profileimage" />
 
 **Email**: [bot protected](mailto:gro.ldnreh@nitram)
 
@@ -15,4 +15,3 @@
 **Public key**: [0xC2461649A6A08BB2](https://herndl.org/pubkey.asc)
 
 **Fingerprint**: 4623 A525 2D4E 7535 CE52  7189 C246 1649 A6A0 8BB2
-
